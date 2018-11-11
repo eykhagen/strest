c3ea392a03f257f92a86c121a9db231b9be26f60
shrikster <shahar@shaharshelly.com>
2018-11-11 15:57:38 +0200

    added back the console log

--- src/handler.ts
+++ src/handler.ts
@@ -55,6 +55,7 @@ export const start = async (dir:string , cmd: any) => {
     console.log();
     return 1;
   }
+  console.log();
   const responseCode = await test.performTests(validTests, cmd) as Number;
   return responseCode;
 }
